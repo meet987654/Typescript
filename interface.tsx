c463d482ed5a8c194bbf5e81266f6b3408650d61
Meet Prajapati <160616727+meet987654@users.noreply.github.com>
2025-08-29 14:55:34 +0530

    Create interface.tsx

---
+++ interface.tsx
@@ -0,0 +1,11 @@
+interface User{
+    name:string;
+    age:number;
+}
+
+function sumOfAge(user1:User,user2:User):number{
+    return user1.age + user2.age;
+}
+
+const result=sumOfAge({name:"Alice",age:30},{name:"Bob",age:25});
+console.log(result);
